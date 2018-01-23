8cdfe9a7a24f28085e0160620ea0e29a3784aef4
Vinovo <martinvictor45@hotmail.com>
2018-01-22 20:46:01 -0600

    Hola Mundo

---
+++ start.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>                     /* Used for (f)printf() functions */
+#include <stdlib.h>                                   /* Used for exit() */
+
+int main (void){
+	int x,y,z; 
+	
+	int *a,b,c; 
+	
+	x = 2;
+	y = 5;
+	z = 10; 
+	
+	a = &x;
+	b = y;
+	
+    return (EXIT_SUCCESS);
+}
